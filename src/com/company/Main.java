2c98689804a5c1eb7c1f3cbd6964e4097c65dcf3
Dmitry Kremlev <kremlev1980@list.ru>
2021-07-07 19:42:53 +0300

    Добавил второй commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("first commit : git commit;");
+
+        System.out.println("second commit");
     }
 }
